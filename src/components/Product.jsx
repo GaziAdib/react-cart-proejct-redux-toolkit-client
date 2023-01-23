00904466e83d98e83a490f643c26e3e549e7e1d1
unknown <adibcse10@gmail.com>
2023-01-24 01:16:21 +0600

    add to cart feature done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { addCart } from '../features/carts/cartsSlice';
 
 const Product = ({ product }) => {
 
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
     const { image, title, price, category } = product || {};
+
+    const addToCart = (product) => {
+        console.log('product', product);
+        dispatch(addCart(product))
+        navigate('/carts');
+    }
+
     return (
 
         <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-4 my-2">
@@ -14,11 +29,12 @@ const Product = ({ product }) => {
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 </a>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{price}</h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-green-600 dark:text-white"><b>{category}</b></h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button onClick={() => addToCart(product)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Add To Cart
                     <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
+                </button>
             </div>
         </div>
 
--- src/features/carts/cartsSlice.js
+++ src/features/carts/cartsSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-
+    cartsItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
+    cartsTotalAmount: 0,
+    cartTotalQuantity: 0
 }
 
 export const cartsSlice = createSlice({
@@ -9,10 +11,45 @@ export const cartsSlice = createSlice({
     initialState,
     reducers: {
 
+        addCart: (state, action) => {
+
+            // if exists increase qty
+
+            let existsIndex = state.cartsItems.findIndex((item) => item._id === action.payload._id);
+
+            if (existsIndex >= 0) {
+                state.cartsItems[existsIndex].qty += 1
+            } else {
+                // add to cart 
+                const assembledItem = { ...action.payload, qty: 1 }
+                state.cartsItems.push(assembledItem);
+
+                // localstorage add
+
+                localStorage.setItem("cartItems", JSON.stringify(state.cartsItems));
+            }
+
+        },
+
+        removeCart: (state, action) => {
+            const updatedCartItems = state.cartsItems.filter((item) => item._id !== action.payload._id)
+
+            state.cartsItems = updatedCartItems;
+
+            // update localstorage ofcart after removing
+            localStorage.setItem("cartItems", JSON.stringify(state.cartsItems));
+        },
+
+        clearCart: (state, action) => {
+
+        }
+
     },
 })
 
 // Action creators are generated for each case reducer function
 // export const { } = cartsSlice.actions
+
+export const { addCart, removeCart, clearCart } = cartsSlice.actions
 
 export default cartsSlice.reducer
