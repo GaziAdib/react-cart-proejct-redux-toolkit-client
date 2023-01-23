1835184eb819e1742f970c13562f3481abf0568c
unknown <adibcse10@gmail.com>
2023-01-23 23:32:00 +0600

    added root api and products api on rtk query part

---
+++ src/features/products/productsApi.js
@@ -0,0 +1,36 @@
+import { rootApi } from "../api/rootApi";
+
+export const productsApi = rootApi.injectEndpoints({
+
+    endpoints: (builder) => ({
+        fetchProducts: builder.query({
+            query: () => '/allProducts'
+        }),
+
+        // create Video
+        addProduct: builder.mutation({
+            query: (data) => ({
+                url: '/addProduct',
+                method: 'POST',
+                body: data
+            }),
+
+            async onQueryStarted(args, { queryFulfilled, dispatch }) {
+                try {
+
+                    const { data: createdProduct } = await queryFulfilled;
+
+                    dispatch(
+                        rootApi.util.updateQueryData('fetchProducts', undefined, (draft) => {
+                            draft?.push(createdProduct);
+                        })
+                    )
+
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }),
+    })
+
+})
