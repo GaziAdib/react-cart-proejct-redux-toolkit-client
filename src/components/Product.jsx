c6c774e95df1d8aeb2084884bb57f480d3dab39f
unknown <adibcse10@gmail.com>
2023-02-02 01:07:16 +0600

    complete new product main card design with grid and flex

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,6 +5,10 @@ import { addCart } from '../features/carts/cartsSlice';
 import { addToWishList } from '../features/wishlists/wishlistSlice';
 import stringSlicer from '../utils/stringSlicer';
 
+import StarIcon from '../assets/images/star.svg';
+import HeartIcon from '../assets/images/love.svg';
+import EyeIcon from '../assets/images/eye.svg';
+
 const Product = ({ product }) => {
 
 
@@ -26,32 +30,119 @@ const Product = ({ product }) => {
 
     return (
 
-        <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-4 my-2">
-            <a href="#">
-                <img className="rounded-t-lg" src={image} alt="" />
-            </a>
-            <div className="p-5">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{stringSlicer(title, 30)}</h5>
-                </a>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{price}</h5>
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-green-600 dark:text-white"><b>{category}</b></h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <button onClick={() => addToCart(product)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Add To Cart
-                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </button>
+        // <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-4 my-2">
+        //     <a href="#">
+        //         <img className="rounded-t-lg" src={image} alt="" />
+        //     </a>
+        //     <div className="p-5">
+        //         <a href="#">
+        //             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{stringSlicer(title, 30)}</h5>
+        //         </a>
+        //         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{price}</h5>
+        //         <h5 className="mb-2 text-2xl font-bold tracking-tight text-green-600 dark:text-white"><b>{category}</b></h5>
+        //         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        //         <button onClick={() => addToCart(product)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        //             Add To Cart
+        //             <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        //         </button>
 
-                <button onClick={() => addToWishHandler(product)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-rose-400 rounded-lg hover:bg-rose-800 focus:ring-4 focus:outline-none">
-                    Add To Wish
-                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </button>
+        //         <button onClick={() => addToWishHandler(product)} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-rose-400 rounded-lg hover:bg-rose-800 focus:ring-4 focus:outline-none">
+        //             Add To Wish
+        //             <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        //         </button>
 
-                <Link to={`/product/${_id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Details
-                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </Link>
+        //         <Link to={`/product/${_id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        //             Details
+        //             <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        //         </Link>
+        //     </div>
+        // </div>
+
+        <div className="bg-white text-gray-700 items-center w-full md:w-4/5 lg:w-full min-h-[10rem] shadow-lg rounded-md overflow-hidden">
+
+            <img className="w-full h-full object-cover" src={image} alt={title} />
+
+            <div className="p-5 flex flex-col gap-3">
+
+                <div className="flex items-center gap-2">
+                    <span className="px-3 py-1 rounded-full text-xs bg-gray-100">Stock Ready</span>
+                    <span className="px-3 py-1 rounded-full text-xs bg-green-100">{category}</span>
+                </div>
+
+
+                {/* Product Title */}
+
+                <h2 className="font-semibold text-2xl overflow-ellipsis overflow-hidden whitespace-nowrap">
+                    {title}
+                </h2>
+
+
+                {/* Product Price section */}
+                <div>
+                    <span className='text-xl font-bold'>
+                        $ {price}
+                    </span>
+
+                    <div className='flex items-center gap-2 mt-1'>
+                        <span className='text-sm line-through opacity-50'>
+                            $ {price}
+                        </span>
+
+                        <span className='bg-green-400 px-1.5 py-0.5 rounded-md text-xs text-white'>
+                            save 20%
+                        </span>
+                    </div>
+
+                </div>
+
+
+                {/* product Rating */}
+
+                <span className='flex items-center mt-1'>
+
+                    <img src={StarIcon} alt="staricon" />
+                    <img src={StarIcon} alt="staricon" />
+                    <img src={StarIcon} alt="staricon" />
+                    <img src={StarIcon} alt="staricon" />
+                    <img src={StarIcon} alt="staricon" />
+
+                    <span className='text-xs ml-2 text-gray-500'>
+                        20k Views
+                    </span>
+
+                </span>
+
+
+                {/* Product Action Button like Cart Wishlist View */}
+
+
+                <div className='mt-5 flex gap-2'>
+
+                    <button onClick={() => addToCart(product)} className='bg-red-500/80 hover:bg-red-500/90 px-6 py-2 rounded-md text-white font-medium tracking-wider transition'>
+                        Add To Cart
+                    </button>
+
+                    <button onClick={() => addToWishHandler(product)} className='flex-grow flex justify-center items-center bg-gray-300/60 hover:bg-gray-300/80 transition rounded-md'>
+                        <img
+                            className='opacity-50'
+                            src={HeartIcon}
+                            alt="add to wishlist" />
+                    </button>
+
+                    <Link to={`/product/${_id}`} className='flex-grow flex justify-center items-center bg-gray-300/60 hover:bg-gray-300/80 transition rounded-md'>
+                        <img
+                            className='opacity-50'
+                            src={EyeIcon}
+                            alt="add to wishlist" />
+                    </Link>
+
+                </div>
+
+
+
+
             </div>
+
         </div>
 
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,12 +9,9 @@ const HomePage = () => {
 
     const { data: products, isLoading, isError, error } = useFetchProductsQuery() || {};
 
-    console.log(products);
-
-
     return (
         <>
-            <h1 className='text-4xl text-center my-4 py-5'>All Products</h1>
+            {/* <h1 className='text-4xl text-center my-4 py-5'>All Products</h1>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 items-center justify-center mx-4 py-5 px-3 my-2'>
                 {
@@ -22,7 +19,27 @@ const HomePage = () => {
                         return <Product key={product?._id} product={product} />
                     })
                 }
+            </div> */}
+
+            {/* <div className='h-screen bg-gray-100 flex items-center flex-row md:flex-row lg:flex-row sm:flex-col justify-center gap-10'>
+
+                {
+                    products?.map((product) => {
+                        return <Product key={product?._id} product={product} />
+                    })
+                }
+            </div> */}
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 items-center justify-center mx-4 py-5 px-3 my-2'>
+
+                {
+                    products?.map((product) => {
+                        return <Product key={product?._id} product={product} />
+                    })
+                }
             </div>
+
+
 
         </>
     )
